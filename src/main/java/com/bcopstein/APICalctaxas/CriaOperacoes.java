7ec28ef36eafc03d57639f1feb528a9a980e4374
KIN MAX PIAMOLINI GUSMAO <16104046@pucrs.br>
2018-08-09 10:55:35 -0300

    Acrescenta o comentário do git

--- src/main/java/com/bcopstein/APICalctaxas/CriaOperacoes.java
+++ src/main/java/com/bcopstein/APICalctaxas/CriaOperacoes.java
@@ -12,6 +12,7 @@ import com.bcopstein.APICalctaxas.persistencia.Persistencia;
 
 public class CriaOperacoes{
     // Argumentos: mes inicial, mes final, ano
+	// Teste do git
     public static void main( String[] args )
     {
         if (args.length != 3){
